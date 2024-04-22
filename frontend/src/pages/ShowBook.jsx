b2fa760ea24617b40036c4d4b94364c3292ec258
monjor-morshed <2020-1-60-227@std.ewubd.edu>
2024-04-22 12:31:15 +0600

    Edit Book Fixed

--- frontend/src/pages/ShowBook.jsx
+++ frontend/src/pages/ShowBook.jsx
@@ -21,7 +21,7 @@ const ShowBook = () => {
   return (
     <div className="p-4">
       <BackButton />
-      <h1 className=" text-3xl my-4"> Show Book</h1>
+      <h1 className=" text-3xl my-4"> Edit Book</h1>
       {loading ? (
         <Spinner />
       ) : (
